a1b38705227e58161747777530ef7a73f327a5b3
tris <nexus4fortune@gmail.com>
2024-01-21 12:15:36 +0900

    リポジトリ名を変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: process.env.GITHUB_ACTIONS ? "/omoji/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/oomoji/" : "/",
 });
