c67879182a95237a01c50b0be792e2578fb77499
tris <nexus4fortune@gmail.com>
2024-01-23 22:36:56 +0900

    文字を大きく表示

--- src/components/TextView.tsx
+++ src/components/TextView.tsx
@@ -1,6 +1,42 @@
+import styled from "styled-components";
+
 import { useValueStore } from "@/misc/store";
+
+const CharsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 export function TextView() {
   const text = useValueStore((state) => state.value);
-  return <div>{text}</div>;
+
+  const array = [];
+
+  for (const s of text) {
+    array.push(<CharView c={s} />);
+  }
+
+  return <CharsWrapper>{array}</CharsWrapper>;
+}
+
+const CharBox = styled.div`
+  border: 1px solid gray;
+  width: fit-content;
+  margin: 1rem;
+  padding: 1rem;
+`;
+const Char = styled.div`
+  font-size: 120px;
+  margin: 0;
+  padding: 0;
+  line-height: 100%;
+`;
+
+function CharView({ c }: { c: string }) {
+  return (
+    <CharBox>
+      <Char>{c}</Char>
+    </CharBox>
+  );
 }
