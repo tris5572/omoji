9db8efd683a9b944a1de60163b9d3073b66200b0
tris <nexus4fortune@gmail.com>
2024-01-24 12:11:26 +0900

    Unicodeを表示

--- src/components/TextView.tsx
+++ src/components/TextView.tsx
@@ -21,22 +21,32 @@ export function TextView() {
 }
 
 const CharBox = styled.div`
-  border: 1px solid gray;
   width: fit-content;
   margin: 1rem;
   padding: 1rem;
+  border: 1px solid gray;
+  border-radius: 8px;
 `;
 const Char = styled.div`
   font-size: 120px;
   margin: 0;
   padding: 0;
   line-height: 100%;
+  text-align: center;
+`;
+const CodeTitle = styled.span`
+  font-size: small;
+`;
+const CodeValue = styled.span`
+  font-family: monospace;
 `;
 
 function CharView({ c }: { c: string }) {
   return (
     <CharBox>
       <Char>{c}</Char>
+      <CodeTitle>Unicode: </CodeTitle>
+      <CodeValue>U+{c.codePointAt(0)?.toString(16).toUpperCase()}</CodeValue>
     </CharBox>
   );
 }
