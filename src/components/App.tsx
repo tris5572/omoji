904b87b53f7812e95c338272b8e4b98d19310875
tris <nexus4fortune@gmail.com>
2024-01-25 20:13:16 +0900

    入力枠を別コンポーネントへ分離

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,14 @@
-import { useRef } from "react";
 import styled, { ThemeProvider } from "styled-components";
 
-import { useSettingStore, useValueStore } from "@/misc/store";
+import { useSettingStore } from "@/misc/store";
 import { TextView } from "./TextView";
 import { DARK_THEME, LIGHT_THEME } from "@/misc/theme";
+import { InputView } from "./InputView";
 
 const Body = styled.div`
   background: ${(p) => p.theme.colors.background.main};
   color: ${(p) => p.theme.colors.text.main};
   height: 100dvh;
-`;
-const InputBox = styled.div`
-  background: ${(p) => p.theme.colors.key.main};
-  padding: 8px;
-  text-align: center;
 `;
 
 function App() {
@@ -21,33 +16,10 @@ function App() {
   const darkFlag = useSettingStore((st) => st.darkFlag);
   const theme = darkFlag ? DARK_THEME : LIGHT_THEME;
 
-  // IMEのオンオフ状態のフラグ。日本語入力中には拡大表示に反映されないようにする。
-  const isIme = useRef(false);
-
-  const changeValue = useValueStore((state) => state.changeValue);
-
-  function handleChange(text: string) {
-    if (isIme.current) {
-      return; // 変換中は何もしない
-    }
-
-    changeValue(text);
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <Body>
-        <InputBox>
-          <input
-            type="text"
-            onChange={(e) => handleChange(e.target.value)}
-            onCompositionStart={() => (isIme.current = true)}
-            onCompositionEnd={(e) => {
-              isIme.current = false;
-              handleChange((e.target as HTMLInputElement).value); // 入力確定時
-            }}
-          />
-        </InputBox>
+        <InputView />
         <TextView />
       </Body>
     </ThemeProvider>
---
+++ src/components/InputView.tsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import styled from "styled-components";
+
+import { useValueStore } from "@/misc/store";
+
+const InputBox = styled.div`
+  background: ${(p) => p.theme.colors.key.main};
+  padding: 8px;
+  text-align: center;
+`;
+
+export function InputView() {
+  // IMEのオンオフ状態のフラグ。日本語入力中には拡大表示に反映されないようにする。
+  const isIme = useRef(false);
+
+  const changeValue = useValueStore((state) => state.changeValue);
+
+  function handleChange(text: string) {
+    if (isIme.current) {
+      return; // 変換中は何もしない
+    }
+
+    changeValue(text);
+  }
+
+  return (
+    <InputBox>
+      <input
+        type="text"
+        onChange={(e) => handleChange(e.target.value)}
+        onCompositionStart={() => (isIme.current = true)}
+        onCompositionEnd={(e) => {
+          isIme.current = false;
+          handleChange((e.target as HTMLInputElement).value); // 入力確定時
+        }}
+      />
+    </InputBox>
+  );
+}
